249c33a16e7174b9d228d757b539657f535feb92
dev <dev@gmail.com>
2019-07-07 12:39:08 +0700

    INIT

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+    <a-layout id="components-layout-demo-custom-trigger">
+        <a-layout-sider
+            :trigger="null"
+            collapsible
+            v-model="collapsed">
+            <div class="logo"/>
+            <a-menu theme="dark" mode="inline" v-model="current">
+                <a-menu-item key="dashboard">
+                    <n-link to="/">
+                        <a-icon type="upload"/>
+                        <span>Dashboard</span>
+                    </n-link>
+                </a-menu-item>
+                <a-menu-item key="task">
+                    <n-link to="/task">
+                        <a-icon type="video-camera"/>
+                        <span>Task</span>
+                    </n-link>
+                </a-menu-item>
+                <a-menu-item key="campaign">
+                    <n-link to="/campaign">
+                        <a-icon type="video-camera"/>
+                        <span>Campaign</span>
+                    </n-link>
+                </a-menu-item>
+                <a-menu-item key="crawler">
+                    <n-link to="/crawler">
+                        <a-icon type="user"/>
+                        <span>Crawler</span>
+                    </n-link>
+                </a-menu-item>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-header style="background: #fff; padding: 0">
+                <a-icon
+                    class="trigger"
+                    :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+                    @click="()=> collapsed = !collapsed"
+                />
+            </a-layout-header>
+            <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', height: 'calc(100vh - 112px)' }">
+                <nuxt/>
+            </a-layout-content>
+        </a-layout>
+    </a-layout>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                collapsed: false,
+                current: [this.$route.path.substr(1)]
+
+            }
+        },
+    }
+</script>
+<style>
+    #components-layout-demo-custom-trigger .trigger {
+        font-size: 18px;
+        line-height: 64px;
+        padding: 0 24px;
+        cursor: pointer;
+        transition: color .3s;
+    }
+
+    #components-layout-demo-custom-trigger .trigger:hover {
+        color: #1890ff;
+    }
+
+    #components-layout-demo-custom-trigger .logo {
+        height: 32px;
+        background: rgba(255, 255, 255, .2);
+        margin: 16px;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+    </div>
+</template>
+
+<script>
+
+    export default {
+        components: {},
+        head() {
+            return {
+                title: 'SupperCrawler'
+            }
+        }
+    }
+</script>
